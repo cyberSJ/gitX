0208120dfd0c8675c587ebde1f16be144709d3e8
cyberSJ <sungjoonkim4.0@gmail.com>
2013-10-29 00:14:06 -0400

    fifth change from gitX

--- gitX.cpp
+++ gitX.cpp
@@ -4,5 +4,6 @@ int main (int argc , char **argv){
 	printf("First edit\n");
 	printf("third change in gitX\n");
 	printf("forth change in gitX2\n");
+	printf("fifth change in git X\n");
 	return 0;
 }
