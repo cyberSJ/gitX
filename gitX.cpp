94367e158586b2aa9d87685fd53d189800fe5b3c
cyberSJ <sungjoonkim4.0@gmail.com>
2013-11-22 04:07:21 -0500

    change in testing

--- gitX.cpp
+++ gitX.cpp
@@ -5,15 +5,6 @@ int main (int argc , char **argv){
 	printf("third change in gitX\n");
 	printf("forth change in gitX2\n");
 	printf("fifth change in git X\n");
-	// hotfix
-	// first branch comment
-	// lindley 1
-	// lindley 2
-	// sung 1
-	// sung 2
-	// sung 3
-	// lindley 2
-	// lindley 3
 	// branch testing
 	return 0;
 }
