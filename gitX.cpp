7d91ab037320bf6eb80735c4c625a61aa931c6fc
cyberSJ <sungjoonkim4.0@gmail.com>
2013-12-04 23:26:31 -0500

    sungBranch added

--- gitX.cpp
+++ gitX.cpp
@@ -5,7 +5,6 @@ int main (int argc , char **argv){
 	printf("third change in gitX\n");
 	printf("forth change in gitX2\n");
 	printf("fifth change in git X\n");
-	// branch testing
-	// weird2
+	// sungBranch
 	return 0;
 }
