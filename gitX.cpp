bbedbcf10f60bc1ee633a97b0df79a85e1e039a5
cyberSJ <sungjoonkim4.0@gmail.com>
2013-12-10 23:43:29 -0500

    change made by weird

--- gitX.cpp
+++ gitX.cpp
@@ -5,6 +5,6 @@ int main (int argc , char **argv){
 	printf("third change in gitX\n");
 	printf("forth change in gitX2\n");
 	printf("fifth change in git X\n");
-	// xBranch
+	// change in weird branch
 	return 0;
 }
